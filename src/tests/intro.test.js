321d51e76742d4f205b2d936e2aba0bb40177af6
LeoZhangYYDS <106665596+LeoZhangYYDS@users.noreply.github.com>
2024-09-26 12:07:58 +1000

    FizzBuzz test

--- src/tests/intro.test.js
+++ src/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../intro";
+import { fizzBuzz, max } from "../intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -15,5 +15,27 @@ describe("max", () => {
   it("should return the first argument if arguments are equal", () => {
     const result = max(2, 2);
     expect(result).toBe(2);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should return FizzBuzz if arguement is divisible by 3 and 5", () => {
+    const result = fizzBuzz(15);
+    expect(result).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if arguement is only divisible by 3 ", () => {
+    const result = fizzBuzz(6);
+    expect(result).toBe("Fizz");
+  });
+
+  it("should return Buzz if arguement is only divisible by  5", () => {
+    const result = fizzBuzz(10);
+    expect(result).toBe("Buzz");
+  });
+
+  it("should return arguement as string if it is not divisible by 3 or 5", () => {
+    const result = fizzBuzz(16);
+    expect(result).toBe("16");
   });
 });
