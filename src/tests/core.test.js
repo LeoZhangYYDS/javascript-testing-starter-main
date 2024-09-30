1494c260c75fd5947704ff8a2bf14a84c29e5f2d
LeoZhangYYDS <106665596+LeoZhangYYDS@users.noreply.github.com>
2024-09-30 12:08:46 +1000

    Parameterized Tests 参数化测试

--- src/tests/core.test.js
+++ src/tests/core.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from "vitest";
-import { getCoupons } from "../core";
+import { calculateDiscount, getCoupons, isPriceInRange } from "../core";
 
 describe("getCoupons", () => {
   it("should return an array of coupons", () => {
@@ -23,5 +23,25 @@ describe("getCoupons", () => {
       expect(coupon.discount).toBeGreaterThan(0);
       expect(coupon.discount).toBeLessThan(1);
     });
+  });
+});
+
+describe("calculateDiscount", () => {
+  it("should return discounted price if given valid code", () => {
+    const result = calculateDiscount(10, "SAVE10");
+    expect(result).toBe(9);
+  });
+});
+
+//Parameterized Tests 参数化测试
+describe("isPriceInRange", () => {
+  it.each([
+    { scenario: "price < min", price: -10, result: false },
+    { scenario: "price > max", price: 200, result: false },
+    { scenario: "price = min", price: 0, result: true },
+    { scenario: "price = max", price: 100, result: true },
+    { scenario: "price between min and max", price: 50, result: true },
+  ])("Should return $result when $scenario", ({ price, result }) => {
+    expect(isPriceInRange(price, 0, 100)).toBe(result);
   });
 });
